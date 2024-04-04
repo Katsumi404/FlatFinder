402439607bf597a2e2bd143302d2d5c4c10306a9
Selin <s.djemal@se22.qmul.ac.uk>
2024-04-04 20:38:57 +0100

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -76,3 +76,117 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
+
+import React, { useState } from 'react';
+import { TextInput, Button, ImageBackground, Text, ScrollView } from 'react-native';
+import InlineTextButton from '../components/inlineTextButton.js';
+import AppStyle from '../styles/AppStyle.js';
+import LoginStyle from '../styles/LoginStyle.js';
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+
+export default function RegisterScreen({ navigation }) {
+  const localImage = require('../assets/background.jpg');
+
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [validationMessage, setValidationMessage] = useState('');
+
+  const handleRegistration = () => {
+    if (password===confirmPassword && password!=="") {
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        navigation.navigate('Main', {user: userCredential.user});
+      })
+      .catch((error) => {
+        setValidationMessage(error.message);
+      });
+    }
+    alert('Registration submitted');
+  };
+
+  const toLogin = () => {
+    navigation.popToTop();
+  }
+
+  const  validateAndSet = (value, valueToCompare, setValue) => {
+    if (value !== valueToCompare) {
+      setValidationMessage("Passwords do not match.");
+    } else {
+      setValidationMessage("");
+    }
+    setValue(value)
+  }
+
+  return (
+    <ImageBackground source={localImage} style={AppStyle.container}>
+      <Text style={LoginStyle.title}>Register</Text>
+      <Text style={{ color: 'white' }}>{validationMessage}</Text>
+        
+      <Text style={LoginStyle.header}>Personal Details</Text>
+      <TextInput 
+        placeholder="Email" 
+        value={email} 
+        onChangeText={setEmail} 
+        style={LoginStyle.input}   
+      />
+      <TextInput 
+        placeholder="Phone Number" 
+        value={phoneNumber} 
+        onChangeText={setPhoneNumber} 
+        style={LoginStyle.input} 
+        keyboardType="phone-pad" 
+      />
+      <TextInput 
+        placeholder="First Name" 
+        value={firstName} 
+        onChangeText={setFirstName} 
+        style={LoginStyle.input}   
+      />
+      <TextInput 
+        placeholder="Last Name" 
+        value={lastName} 
+        onChangeText={setLastName} 
+        style={LoginStyle.input}
+      />
+      <TextInput 
+        placeholder="Date of Birth" 
+        value={dateOfBirth} 
+        onChangeText={setDateOfBirth} 
+        style={LoginStyle.input} 
+      />
+        
+      <Text style={LoginStyle.header}>Set Password</Text>
+      
+      <TextInput 
+        placeholder="Password" 
+        value={password} 
+        onChangeText={(value) => validateAndSet(value, confirmPassword, setPassword)} 
+        style={LoginStyle.input} 
+        secureTextEntry={true} 
+      />
+      <TextInput 
+        placeholder="Confirm Password" 
+        value={confirmPassword} 
+        onChangeText={(value) => validateAndSet(value, password, setConfirmPassword)} 
+        style={LoginStyle.input} 
+        secureTextEntry={true} 
+      />
+
+      <Button 
+        title="Register" 
+        onPress={handleRegistration} 
+        style={LoginStyle.button}
+        color='green'  
+      />
+      <Text style={{ color: '#77DD77' }}>Already have an account? <InlineTextButton text="Login" onPress={toLogin}/></Text>
+    </ImageBackground>
+  );
+}
