a063db1f334e01ab2e4bf0620e1538ea05b4e8cc
ec21636 <163432492+ec21636@users.noreply.github.com>
2024-04-05 16:19:13 +0100

    Create MatchmakingStyle.js

---
+++ styles/MatchmakingStyle.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native-web";
+
+export default StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 40,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 20,
+  },
+  matchListContainer: {
+    marginTop: 20,
+  },
+  matchListTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  flatItem: {
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+});
